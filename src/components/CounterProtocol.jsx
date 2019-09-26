31cfa9bb6d3d4f25790fdc0322455dfa2fde9765
assnctr <trollplacez@gmail.com>
2019-09-26 16:16:33 +0300

    Added protocol component

---
+++ src/components/CounterProtocol.jsx
@@ -0,0 +1,17 @@
+import React, { memo } from 'react';
+import { splitByKK } from '../misc/text';
+
+const CounterProtocol = memo(({ count, name, className }) => {
+    if (count == undefined) return null;
+
+    return (
+        <div className={`protocol ${className} ${count > 0 ? 'active' : ''}`}>
+            <div className="protocol-wrap">
+                <div className="type">{name}</div>
+                <div className="count">{splitByKK(count)}</div>
+            </div>
+        </div>
+    );
+});
+
+export default CounterProtocol;
