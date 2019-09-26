538468bb6e5b77796c445e10ebe277317b2b4b20
assnctr <trollplacez@gmail.com>
2019-09-26 16:04:04 +0300

    Added stats

--- src/store/reducers/main.js
+++ src/store/reducers/main.js
@@ -1,11 +1,17 @@
 import { initial } from '../../core/settings';
-import { MAIN_TOGGLE_DARK } from '../../constants/ActionTypes';
+import { MAIN_TOGGLE_DARK, MAIN_SET_STATS } from '../../constants/ActionTypes';
 
 const main = (state = initial.main, action) => {
     switch (action.type) {
         case MAIN_TOGGLE_DARK:
             return {
+                ...state,
                 dark: !state.dark
+            };
+        case MAIN_SET_STATS:
+            return {
+                ...state,
+                stats: action.stats
             };
         default:
             return state;
