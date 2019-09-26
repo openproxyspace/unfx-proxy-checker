317d380237daaadf8558f3c6707f241a44d700c6
assnctr <trollplacez@gmail.com>
2019-09-26 16:11:38 +0300

    Added max threads controller

--- src/misc/other.js
+++ src/misc/other.js
@@ -1,6 +1,20 @@
-import { shell } from "electron";
+import { shell } from 'electron';
 
 export const openLink = e => {
     e.preventDefault();
     shell.openExternal(e.currentTarget.href);
+};
+
+export const getMaxThreads = protocols => {
+    const enabledProtocols = Object.keys(protocols).filter(protocol => protocols[protocol]);
+
+    if (enabledProtocols.length > 3) {
+        return 500;
+    } else if (enabledProtocols.length > 2) {
+        return 650;
+    } else if (enabledProtocols.length > 1) {
+        return 1000;
+    } else {
+        return 1850;
+    }
 };
